3a83132fe8a6fecde70439c88ec55df3f9138616
mprad64 <mateusprado@protonmail.com>
2022-08-07 12:40:53 -0300

    Create medias.py

---
+++ Exercicios/medias.py
@@ -0,0 +1,27 @@
+""" Exercício 2
+Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética. Observe o exemplo abaixo:
+
+Exemplo:
+
+Entrada de Dados:
+
+Digite a primeira nota: 4
+
+Digite a segunda nota: 5
+
+Digite a terceira nota: 6
+
+Digite a quarta nota: 7
+
+Saída de Dados:
+
+A média aritmética é 5.5 """
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"A média aritmética é {media:.2f}")
