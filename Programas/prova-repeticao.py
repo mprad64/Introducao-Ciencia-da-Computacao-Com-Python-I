955dc4c757535df878278d4079ad70c8e27b9a58
mprad64 <mateusprado@protonmail.com>
2022-08-10 14:30:27 -0300

    Aula e prova (repetição com while)
    os exercícios da prova estão em forma de comentário porque fui fazendo ao longo do tempo

---
+++ Programas/prova-repeticao.py
@@ -0,0 +1,50 @@
+""" 1 O que será impresso pelo trecho abaixo? 
+
+x = 1
+while (x < 4):
+    x = x + 1
+print(f"x vale {x}") """
+
+""" 2 O que será impresso pelo trecho de código abaixo? 
+
+Observação: na impressão um número ficará abaixo do outro e não ao lado. 
+
+i = 0
+while (i < 5):
+    print(i)
+    i = i + 1 """
+
+""" 3 O que será impresso pelo código abaixo? 
+
+
+Observação 1: na impressão um número ficará abaixo do outro e não ao lado. 
+Observação 2: i += 2 é equivalente a i = i + 2.   
+
+i = 2
+while i % 3 != 0:
+    print(i)
+    i += 2 """
+
+""" 4. Analise cuidadosamente o código e assinale a alternativa correta:
+
+count = 0
+while (count < 10):
+    # ponto A
+    print(f"Olá... {count}")
+    count =+ 1
+    # Ponto B
+# Ponto C """
+
+""" 5.
+O que será impresso quando o trecho abaixo for executado? 
+
+Observação: na impressão um número ficará abaixo do outro e não ao lado.   
+
+from re import I
+
+
+i = 6
+while (i > 0):
+    i = i - 3
+    print(i)
+"""
