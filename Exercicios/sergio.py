61d09a5e9e19e6bc31daad2352381f77b29e3a92
mprad64 <mateusprado@protonmail.com>
2023-05-12 09:42:26 -0300

    Update sergio.py
    sei la oq é, so tava na pool

--- Exercicios/sergio.py
+++ Exercicios/sergio.py
@@ -1,2 +1,16 @@
-nome = str(input("Digite seu nome: "))
-print(f"olá, {nome}")
+# Lê o número de notas
+n = int(input("Insira o número de notas: "))
+
+# Inicializa a variável de soma com 0
+soma = 0
+
+# Lê as notas e as adiciona à soma
+for i in range(n):
+  nota = float(input("Insira a nota: "))
+  soma += nota
+
+# Calcula a média aritmética
+media = soma / n
+
+# Imprime o resultado
+print("A média aritmética é", media)
